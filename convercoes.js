8914e3411b18ec06c34ae65110c97a00801f4a32
DaniF-Lima <danielferreiradelima22@gmail.com>
2023-12-21 08:38:41 -0300

    codigos var e conversoes

---
+++ convercoes.js
@@ -0,0 +1,17 @@
+// tipo de dado
+// looleanos
+
+// conversão implícita
+const numero = 547;
+const numeroString = '547';
+
+console.log(numero == numeroString);
+console.log(numero + Number(numeroString));
+console.log(String(numero) + numeroString);
+
+// Number()
+// String()
+// os 3 === comparam não apenas o valor mas tbm o tipo
+
+// conversão explícita
+
